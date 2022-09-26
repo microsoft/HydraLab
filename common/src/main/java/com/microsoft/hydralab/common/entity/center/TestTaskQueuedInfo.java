d74909e58cc2c253a9f1626c2e8b7277f4845bba
Le Zhou <2428499107@qq.com>
2022-09-26 17:05:52 +0800

    Revert "add: push code to github"
    This reverts commit dcf79ffa6526f46a5cd4e7928e489cd19846a082.

--- common/src/main/java/com/microsoft/hydralab/common/entity/center/TestTaskQueuedInfo.java
+++
@@ -1,9 +0,0 @@
-package com.microsoft.hydralab.common.entity.center;
-
-import lombok.Data;
-
-@Data
-public class TestTaskQueuedInfo {
-    private TestTaskSpec testTaskSpec;
-    private int[] queuedInfo = new int[2];
-}
