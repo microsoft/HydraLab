3b23c388c62b182ae95e14870a3b8b860944551f
TedaLIEz <aliezted@gmail.com>
2022-09-27 10:23:03 +0800

    feat: migrate appium test runner code

---
+++ agent/src/main/java/com/microsoft/hydralab/agent/runner/appium/AppiumListener.java
@@ -0,0 +1,253 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+package com.microsoft.hydralab.agent.runner.appium;
+
+import cn.hutool.core.img.ImgUtil;
+import cn.hutool.core.img.gif.AnimatedGifEncoder;
+import com.microsoft.hydralab.common.entity.common.AndroidTestUnit;
+import com.microsoft.hydralab.common.entity.common.DeviceInfo;
+import com.microsoft.hydralab.common.entity.common.DeviceTestTask;
+import com.microsoft.hydralab.common.logger.LogCollector;
+import com.microsoft.hydralab.common.management.DeviceManager;
+import com.microsoft.hydralab.common.screen.ScreenRecorder;
+import org.junit.runner.Description;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+import org.junit.runner.notification.RunListener;
+import org.slf4j.Logger;
+
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
+public class AppiumListener extends RunListener {
+    private final DeviceInfo deviceInfo;
+    private final DeviceTestTask deviceTestResult;
+    private final LogCollector logcatCollector;
+    private final ScreenRecorder deviceScreenRecorder;
+    private final Logger logger;
+    private final AnimatedGifEncoder e = new AnimatedGifEncoder();
+    private final String pkgName;
+    DeviceManager deviceManager;
+    private long recordingStartTimeMillis;
+    private int index;
+    private File gifFile;
+    private boolean alreadyEnd = false;
+    private AndroidTestUnit ongoingTestUnit;
+    private int numTests;
+    private int pid;
+    private int addedFrameCount;
+    private String currentTestName = "";
+    private int currentTestIndex = 0;
+
+
+    public AppiumListener(DeviceManager deviceManager, DeviceInfo deviceInfo, DeviceTestTask deviceTestResult, String pkgName, Logger logger) {
+        this.deviceManager = deviceManager;
+        this.deviceInfo = deviceInfo;
+        this.deviceTestResult = deviceTestResult;
+        this.logger = logger;
+        this.pkgName = pkgName;
+        logcatCollector = deviceManager.getLogCollector(deviceInfo, pkgName, deviceTestResult, logger);
+        deviceScreenRecorder = deviceManager.getScreenRecorder(deviceInfo, deviceTestResult.getDeviceTestResultFolder(), logger);
+    }
+
+    public File getGifFile() {
+        return gifFile;
+    }
+
+    private void logEnter(Object... args) {
+        StringBuilder builder = new StringBuilder();
+        for (Object arg : args) {
+            builder.append(" >").append(arg);
+        }
+        logger.info("TestRunListener: {}", builder);
+        System.out.println("TestRunListener: " + builder);
+
+    }
+
+    public void startRecording(int maxTime) {
+        startTools();
+        logger.info("Start record screen");
+        deviceScreenRecorder.setupDevice();
+        deviceScreenRecorder.startRecord(maxTime <= 0 ? 30 * 60 : maxTime);
+        recordingStartTimeMillis = System.currentTimeMillis();
+        final String initializing = "Initializing";
+        deviceInfo.setRunningTestName(initializing);
+        deviceTestResult.addNewTimeTag(initializing, 0);
+    }
+
+    private void startTools() {
+//        try {
+//            pid = adbDeviceManager.getPackagePid(deviceInfo, pkgName, logger);
+//            logger.info("{} is running test with pid {}", pkgName, pid);
+//        } catch (Exception exception) {
+//            exception.printStackTrace();
+//        }
+        logger.info("Start gif frames collection");
+        gifFile = new File(deviceTestResult.getDeviceTestResultFolder(), pkgName + ".gif");
+        e.start(gifFile.getAbsolutePath());
+        e.setDelay(1000);
+        e.setRepeat(0);
+
+        logger.info("Start logcat collection");
+        String logcatFilePath = logcatCollector.start();
+        deviceTestResult.setLogcatPath(deviceManager.getTestBaseRelPathInUrl(new File(logcatFilePath)));
+    }
+
+    @Override
+    public void testRunStarted(Description description) {
+        String runName = description.getChildren().get(0).getDisplayName();
+        int testCount = description.getChildren().get(0).testCount();
+        logEnter("testRunStarted", runName, testCount);
+        this.numTests = description.testCount();
+        deviceTestResult.setTotalCount(testCount);
+        deviceTestResult.setTestStartTimeMillis(System.currentTimeMillis());
+        deviceTestResult.addNewTimeTag("testRunStarted", System.currentTimeMillis() - recordingStartTimeMillis);
+        deviceInfo.setRunningTestName(runName.substring(runName.lastIndexOf('.') + 1) + ".testRunStarted");
+        logEnter(runName, description.testCount());
+    }
+
+    @Override
+    public void testStarted(Description description) {
+        String testName = description.getMethodName() == null ? "testInitialization" : description.getMethodName();
+        String testClassName = description.getClassName();
+        String testDisplayName = description.getDisplayName();
+        int testIndex;
+
+        if (!testClassName.equals(currentTestName)) {
+            currentTestName = testClassName;
+        }
+
+        testIndex = ++currentTestIndex;
+
+        logEnter("testStarted", testDisplayName);
+        ongoingTestUnit = new AndroidTestUnit();
+        ongoingTestUnit.setNumtests(testIndex);
+
+        ongoingTestUnit.setStartTimeMillis(System.currentTimeMillis());
+        ongoingTestUnit.setRelStartTimeInVideo(ongoingTestUnit.getStartTimeMillis() - recordingStartTimeMillis);
+
+        final int unitIndex = testIndex;
+        ongoingTestUnit.setCurrentIndexNum(unitIndex);
+
+//        String method = test.getTestName();
+        ongoingTestUnit.setTestName(testName);
+
+        ongoingTestUnit.setTestedClass(testClassName);
+
+        deviceTestResult.addNewTimeTag(unitIndex + ". " + ongoingTestUnit.getTitle(), System.currentTimeMillis() - recordingStartTimeMillis);
+        deviceInfo.setRunningTestName(ongoingTestUnit.getTitle());
+
+        ongoingTestUnit.setDeviceTestResultId(deviceTestResult.getId());
+        ongoingTestUnit.setTestTaskId(deviceTestResult.getTestTaskId());
+
+        deviceTestResult.addNewTestUnit(ongoingTestUnit);
+
+        deviceManager.updateScreenshotImageAsyncDelay(deviceInfo, TimeUnit.SECONDS.toMillis(15), (imagePNGFile -> {
+            if (imagePNGFile == null) {
+                return;
+            }
+            if (!e.isStarted()) {
+                return;
+            }
+            try {
+                e.addFrame(ImgUtil.toBufferedImage(ImgUtil.scale(ImageIO.read(imagePNGFile), 0.3f)));
+                addedFrameCount++;
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+        }), logger);
+    }
+
+    @Override
+    public void testFailure(Failure failure) {
+        String testDisplayName = failure.getDescription().getDisplayName();
+        logEnter("testFailed", testDisplayName, failure.getTrace());
+        ongoingTestUnit.setStack(failure.getTrace());
+        ongoingTestUnit.setStatusCode(AndroidTestUnit.StatusCodes.FAILURE);
+        deviceTestResult.addNewTimeTag(ongoingTestUnit.getTitle() + ".fail", System.currentTimeMillis() - recordingStartTimeMillis);
+        deviceTestResult.oneMoreFailure();
+    }
+
+    @Override
+    public void testAssumptionFailure(Failure failure) {
+        String testDisplayName = failure.getDescription().getDisplayName();
+        logEnter("testAssumptionFailure", testDisplayName, failure.getTrace());
+        ongoingTestUnit.setStack(failure.getTrace());
+        ongoingTestUnit.setStatusCode(AndroidTestUnit.StatusCodes.FAILURE);
+        deviceTestResult.addNewTimeTag(ongoingTestUnit.getTitle() + ".assumptionFail", System.currentTimeMillis() - recordingStartTimeMillis);
+        deviceTestResult.oneMoreFailure();
+    }
+
+    @Override
+    public void testIgnored(Description description) {
+        logEnter("testIgnored", description.getDisplayName());
+        ongoingTestUnit.setStatusCode(AndroidTestUnit.StatusCodes.IGNORED);
+    }
+
+    @Override
+    public void testFinished(Description description) {
+        logEnter("testEnded", description.getDisplayName());
+        deviceTestResult.addNewTimeTag(ongoingTestUnit.getTitle() + ".end", System.currentTimeMillis() - recordingStartTimeMillis);
+        if (ongoingTestUnit.getStatusCode() == 0
+                || ongoingTestUnit.getStatusCode() == AndroidTestUnit.StatusCodes.ASSUMPTION_FAILURE
+                || ongoingTestUnit.getStatusCode() == AndroidTestUnit.StatusCodes.IGNORED
+        ) {
+            ongoingTestUnit.setStatusCode(AndroidTestUnit.StatusCodes.OK);
+            ongoingTestUnit.setSuccess(true);
+        }
+        ongoingTestUnit.setEndTimeMillis(System.currentTimeMillis());
+        ongoingTestUnit.setRelEndTimeInVideo(ongoingTestUnit.getEndTimeMillis() - recordingStartTimeMillis);
+    }
+
+    @Override
+    public void testRunFinished(Result result) {
+        long elapsedTime = result.getRunTime();
+        boolean wasSuccessful = result.wasSuccessful();
+
+        if (wasSuccessful) {
+            logEnter("testRunSuccessful", elapsedTime, Thread.currentThread().getName());
+            deviceTestResult.addNewTimeTag("testRunSuccessful", System.currentTimeMillis() - recordingStartTimeMillis);
+        } else {
+            String errorMessage = result.getFailures().get(0).getMessage();
+            logEnter("testRunFailed", errorMessage);
+            deviceTestResult.addNewTimeTag("testRunFailed", System.currentTimeMillis() - recordingStartTimeMillis);
+            deviceTestResult.setTestErrorMessage(errorMessage);
+            if (errorMessage != null && errorMessage.toLowerCase(Locale.US).contains("process crash")) {
+                if (deviceTestResult.getCrashStack() == null) {
+                    deviceTestResult.setCrashStack(errorMessage);
+                }
+            }
+            if (e.isStarted() && addedFrameCount < 2) {
+                try {
+                    File imagePNGFile = deviceManager.getScreenShot(deviceInfo, logger);
+                    e.addFrame(ImgUtil.toBufferedImage(ImgUtil.scale(ImageIO.read(imagePNGFile), 0.3f)));
+                } catch (Exception exception) {
+                    logger.error(exception.getMessage(), e);
+                }
+            }
+
+        }
+
+        logEnter("testRunEnded", elapsedTime, Thread.currentThread().getName());
+        synchronized (this) {
+            if (alreadyEnd) {
+                return;
+            }
+            deviceTestResult.addNewTimeTag("testRunEnded", System.currentTimeMillis() - recordingStartTimeMillis);
+            deviceTestResult.onTestEnded();
+            deviceInfo.setRunningTestName(null);
+            releaseResource();
+            alreadyEnd = true;
+        }
+    }
+
+    private void releaseResource() {
+        e.finish();
+        deviceScreenRecorder.finishRecording();
+        logcatCollector.stopAndAnalyse();
+        logger.info("Record Finished");
+    }
+}
---
+++ agent/src/main/java/com/microsoft/hydralab/agent/runner/appium/AppiumRunner.java
@@ -0,0 +1,200 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+package com.microsoft.hydralab.agent.runner.appium;
+
+import com.microsoft.hydralab.appium.AppiumParam;
+import com.microsoft.hydralab.appium.ThreadParam;
+import com.microsoft.hydralab.common.entity.common.DeviceInfo;
+import com.microsoft.hydralab.common.entity.common.DeviceTestTask;
+import com.microsoft.hydralab.common.entity.common.TestTask;
+import com.microsoft.hydralab.common.management.impl.IOSDeviceManager;
+import com.microsoft.hydralab.agent.runner.RunningControlService;
+import com.microsoft.hydralab.agent.runner.TestRunner;
+import com.microsoft.hydralab.agent.runner.TestRunningCallback;
+import com.microsoft.hydralab.common.util.IOSUtils;
+import org.junit.internal.TextListener;
+import org.junit.platform.launcher.Launcher;
+import org.junit.platform.launcher.LauncherDiscoveryRequest;
+import org.junit.platform.launcher.TestExecutionListener;
+import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
+import org.junit.platform.launcher.core.LauncherFactory;
+import org.junit.runner.JUnitCore;
+import org.slf4j.Logger;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.platform.engine.discovery.DiscoverySelectors.selectClass;
+
+@Service
+public class AppiumRunner extends TestRunner {
+
+    @Override
+    public RunningControlService.DeviceTask getDeviceTask(TestTask testTask, TestRunningCallback testRunningCallback) {
+
+        return (deviceInfo, logger) -> {
+            runAppiumTest(testTask.getTestAppFile(), testTask.getTestSuite(), deviceInfo, testTask, testRunningCallback, logger);
+            return true;
+        };
+    }
+
+    public void runAppiumTest(File appiumJarFile, String appiumCommand, DeviceInfo deviceInfo,
+                              TestTask testTask,
+                              TestRunningCallback testRunningCallback, Logger logger) {
+        checkTestTaskCancel(testTask);
+        logger.info("Start running tests {}, timeout {}s", testTask.getTestSuite(), testTask.getTimeOutSecond());
+
+        DeviceTestTask deviceTestTask = initDeviceTestTask(deviceInfo, testTask, logger);
+        File deviceTestResultFolder = deviceTestTask.getDeviceTestResultFolder();
+        testTask.addTestedDeviceResult(deviceTestTask);
+        checkTestTaskCancel(testTask);
+
+        Logger reportLogger = null;
+
+        try {
+            reportLogger = initReportLogger(deviceTestTask, testTask, logger);
+            initDevice(deviceInfo, testTask, reportLogger);
+
+            File gifFile = runAndGetGif(appiumJarFile, appiumCommand, deviceInfo, testTask, deviceTestTask, deviceTestResultFolder, reportLogger);
+            if (gifFile != null && gifFile.exists() && gifFile.length() > 0) {
+                deviceTestTask.setTestGifPath(deviceManager.getTestBaseRelPathInUrl(gifFile));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (reportLogger != null) {
+                reportLogger.info(deviceInfo.getSerialNum() + ": " + e.getMessage(), e);
+            } else {
+                logger.info(deviceInfo.getSerialNum() + ": " + e.getMessage(), e);
+            }
+            String errorStr = e.getClass().getName() + ": " + e.getMessage();
+            if (errorStr.length() > 255) {
+                errorStr = errorStr.substring(0, 254);
+            }
+            deviceTestTask.setErrorInProcess(errorStr);
+        } finally {
+            //clear config
+            ThreadParam.clean();
+            afterTest(deviceInfo, testTask, deviceTestTask, testRunningCallback, reportLogger);
+        }
+    }
+
+    @Override
+    protected void afterTest(DeviceInfo deviceInfo, TestTask testTask, DeviceTestTask deviceTestTask, TestRunningCallback testRunningCallback, Logger reportLogger) {
+        quitAppiumDrivers(deviceInfo, testTask, reportLogger);
+        super.afterTest(deviceInfo, testTask, deviceTestTask, testRunningCallback, reportLogger);
+    }
+
+    protected void quitAppiumDrivers(DeviceInfo deviceInfo, TestTask testTask, Logger reportLogger) {
+        deviceManager.quitMobileAppiumDriver(deviceInfo, reportLogger);
+    }
+
+    protected File runAndGetGif(File appiumJarFile, String appiumCommand, DeviceInfo deviceInfo, TestTask testTask, DeviceTestTask deviceTestTask, File deviceTestResultFolder, Logger reportLogger) {
+        //set appium test property
+        reportLogger.info("Start set appium test property");
+        Map<String, String> instrumentationArgs = testTask.getInstrumentationArgs();
+        if (instrumentationArgs == null) {
+            instrumentationArgs = new HashMap<>();
+        }
+        AppiumParam appiumParam = new AppiumParam(deviceInfo.getSerialNum(), deviceInfo.getName(), deviceInfo.getOsVersion(), IOSUtils.getWdaPortByUdid(deviceInfo.getSerialNum(), reportLogger), testTask.getAppFile().getAbsolutePath(), deviceTestResultFolder.getAbsolutePath());
+        ThreadParam.init(appiumParam, instrumentationArgs);
+        reportLogger.info("ThreadParam init success, AppiumParam is " + appiumParam.toString() + ", args is " + instrumentationArgs.toString());
+        File gifFile = null;
+        if (TestTask.TestFrameworkType.JUNIT5.equals(testTask.getFrameworkType())) {
+            reportLogger.info("Start init listener");
+            Junit5Listener junit5Listener = new Junit5Listener(deviceManager, deviceInfo, deviceTestTask, testTask.getPkgName(), reportLogger);
+
+            /** run the test */
+            reportLogger.info("Start appium test with junit5");
+            junit5Listener.startRecording(testTask.getTimeOutSecond());
+            checkTestTaskCancel(testTask);
+            startJunit5(appiumJarFile, appiumCommand, junit5Listener, reportLogger);
+            checkTestTaskCancel(testTask);
+            gifFile = junit5Listener.getGifFile();
+        } else {
+            /** xml report: parse listener */
+            reportLogger.info("Start init listener");
+            AppiumListener listener = new AppiumListener(deviceManager, deviceInfo, deviceTestTask, testTask.getPkgName(), reportLogger);
+            AntXmlRunListener xmlListener = new AntXmlRunListener(deviceTestResultFolder);
+
+            /** run the test */
+            reportLogger.info("Start appium test with junit4");
+            listener.startRecording(testTask.getTimeOutSecond());
+            checkTestTaskCancel(testTask);
+            startJunit(appiumJarFile, appiumCommand, listener, xmlListener, reportLogger);
+            checkTestTaskCancel(testTask);
+            gifFile = listener.getGifFile();
+        }
+        /** set paths */
+        String absoluteReportPath = deviceTestResultFolder.getAbsolutePath();
+        deviceTestTask.setTestXmlReportPath(deviceManager.getTestBaseRelPathInUrl(new File(absoluteReportPath)));
+        return gifFile;
+    }
+
+    public boolean startJunit(File appiumJarFile, String appiumCommand, AppiumListener listener, AntXmlRunListener xmlListener, Logger logger) {
+        JUnitCore junit = new JUnitCore();
+        junit.addListener(new TextListener(System.out));
+        junit.addListener(listener);
+        junit.addListener(xmlListener);
+        URL url = null;
+        try {
+            url = new URL("file:" + appiumJarFile);
+        } catch (MalformedURLException e) {
+            logger.error("runAppiumTest error", e);
+        }
+        URLClassLoader urlClassLoader = new URLClassLoader(new URL[]{url}, Thread.currentThread().getContextClassLoader());
+        Class<?> myClass;
+        try {
+            myClass = urlClassLoader.loadClass(appiumCommand);
+            junit.run(myClass);
+            return true;
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean startJunit5(File appiumJarFile, String appiumCommand, TestExecutionListener listener, Logger logger) {
+        Launcher launcher = LauncherFactory.create();
+
+        URL url = null;
+        try {
+            url = new URL("file:" + appiumJarFile);
+        } catch (MalformedURLException e) {
+            logger.error("runAppiumTest error", e);
+        }
+        URLClassLoader urlClassLoader = new URLClassLoader(new URL[]{url}, Thread.currentThread().getContextClassLoader());
+        Class<?> myClass;
+        try {
+            myClass = urlClassLoader.loadClass(appiumCommand);
+            LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder.request()
+                    .selectors(selectClass(myClass))
+                    .build();
+            launcher.discover(request);
+            launcher.registerTestExecutionListeners(listener);
+            launcher.execute(request);
+            return true;
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @Override
+    public void reInstallApp(DeviceInfo deviceInfo, TestTask testTask, Logger reportLogger) throws Exception {
+        if (testTask.getRequireReinstall() || deviceManager instanceof IOSDeviceManager) {
+            deviceManager.uninstallApp(deviceInfo, testTask.getPkgName(), reportLogger);
+            deviceManager.safeSleep(1000);
+        } else if (testTask.getRequireClearData()) {
+            deviceManager.resetPackage(deviceInfo, testTask.getPkgName(), reportLogger);
+        }
+        checkTestTaskCancel(testTask);
+
+        deviceManager.installApp(deviceInfo, testTask.getAppFile().getAbsolutePath(), reportLogger);
+    }
+}
