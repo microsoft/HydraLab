344565ed5ae2d7855cdc577ded689f1f5e3c4e3f
hodin <hodin@microsoft.com>
2022-10-24 14:45:39 +0800

    update

--- taps_to_cases/T2C_Runner/src/main/java/com/microsoft/hydralab/t2c/runner/controller/WindowsDriverController.java
+++ taps_to_cases/T2C_Runner/src/main/java/com/microsoft/hydralab/t2c/runner/controller/WindowsDriverController.java
@@ -3,9 +3,6 @@
 package com.microsoft.hydralab.t2c.runner.controller;
 
 import io.appium.java_client.windows.WindowsDriver;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.Point;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.PointerInput;
 import org.openqa.selenium.interactions.Sequence;
 import org.slf4j.Logger;
@@ -22,7 +19,8 @@ public class WindowsDriverController extends BaseDriverController {
 
     @Override
     public void dragAndDropWithPosition(int fromX, int fromY, int toX, int toY) {
-        PointerInput finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");
+        // Using "Pen" on Windows Platform can run Drag and Drop well since Mouse is not supported in current appium version
+        PointerInput finger = new PointerInput(PointerInput.Kind.PEN, "PEN");
         Sequence dragNDrop = new Sequence(finger, 1);
         dragNDrop.addAction(finger.createPointerMove(Duration.ofMillis(0),
                 PointerInput.Origin.viewport(), fromX, fromY));
