41eb565e77d451cba1bc4b6c3f24921b17eacf2d
Le Zhou <2428499107@qq.com>
2022-10-27 17:50:46 +0800

    The file can't run normally and regenerate it (#95)
    * regenerate start.sh file with unix format

--- center/deploy_startup/start.sh
+++ center/deploy_startup/start.sh
@@ -1,6 +1,5 @@
 cd /opt/grafana-9.0.1/bin/
 nohup ./grafana-server &
-
 nohup /opt/pushgateway-1.4.3.linux-amd64/pushgateway >> /opt/mount_data/logs/pushgateway/pushgateway_current.log &
 nohup /opt/prometheus-2.36.2.linux-amd64/prometheus --config.file=/opt/prometheus-2.36.2.linux-amd64/prometheus.yml --storage.tsdb.path=/opt/mount_data/prometheus_data --storage.tsdb.retention.time=1y >> /opt/mount_data/logs/prometheus/prometheus_current.log &
 /usr/sbin/nginx &
