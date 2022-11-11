c313e7b9980b8f4c95ff7af09a5a1445c2f4b872
Ran Tao <1252026067@qq.com>
2022-11-11 17:09:13 +0800

    Support click by coordinates for T2C runner

--- taps_to_cases/T2C_Runner/src/main/java/com/microsoft/hydralab/t2c/runner/controller/EdgeDriverController.java
+++ taps_to_cases/T2C_Runner/src/main/java/com/microsoft/hydralab/t2c/runner/controller/EdgeDriverController.java
@@ -2,7 +2,11 @@ package com.microsoft.hydralab.t2c.runner.controller;
 
 import io.appium.java_client.windows.WindowsDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.interactions.PointerInput;
+import org.openqa.selenium.interactions.Sequence;
 import org.slf4j.Logger;
+
+import java.util.Arrays;
 
 public class EdgeDriverController extends BaseDriverController {
     EdgeDriver edgeDriver;
@@ -17,5 +21,14 @@ public class EdgeDriverController extends BaseDriverController {
     @Override
     public void switchToUrl(String url) {
         edgeDriver.get(url);
+    }
+
+    @Override
+    public void tap(int x, int y) {
+        PointerInput finger = new PointerInput(PointerInput.Kind.PEN, "PEN");
+        Sequence click = new Sequence(finger, 1);
+        click.addAction(finger.createPointerDown(PointerInput.MouseButton.LEFT.asArg()));
+        finger.createPointerUp(PointerInput.MouseButton.LEFT.asArg());
+        windowsDriver.perform(Arrays.asList(click));
     }
 }
