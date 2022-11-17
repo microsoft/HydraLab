f531230f63b52d478476569d3b84b5ac7fdb3e85
RaccoonLi <88028886+RaccoonLi@users.noreply.github.com>
2022-11-17 10:12:41 +0800

    User/yingjie/front end (#149)
    * Fix some front end bugs

--- react/src/component/RunnerView.jsx
+++ react/src/component/RunnerView.jsx
@@ -858,6 +858,10 @@ export default class RunnerView extends BaseView {
     }
 
     uploadApk = () => {
+        if (!this.state.selectedTeamName || !this.state.uploadAppInstallerFile) {
+            this.snackBarMsg("Please upload APK/IPA file and select a team")
+            return
+        }
         const formData = new FormData()
         formData.append("teamName", this.state.selectedTeamName)
         formData.append("appFile", this.state.uploadAppInstallerFile)
@@ -1088,6 +1092,7 @@ export default class RunnerView extends BaseView {
     }
 
     componentDidMount() {
+        this.getUserInfo()
         this.refreshPackageList()
         this.refreshRunnableList()
         this.refreshTeamList()
