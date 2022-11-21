3324e56e5d3e375ed264b95bfc02d7bb5f58b0b1
Le Zhou <2428499107@qq.com>
2022-11-21 14:12:06 +0800

    Modify sample T2C test to adapt to no Appium Env (#155)
    * Modify sample T2C test to adapt to no Appium Env
    
    * delete exception throwing

--- taps_to_cases/T2C_Runner/src/test/java/SampleT2CTest.java
+++ taps_to_cases/T2C_Runner/src/test/java/SampleT2CTest.java
@@ -12,18 +12,15 @@ import io.appium.java_client.service.local.flags.GeneralServerFlag;
 import io.appium.java_client.windows.WindowsDriver;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-@Disabled
 public class SampleT2CTest {
 
     public AndroidDriver driver;
@@ -38,18 +35,22 @@ public class SampleT2CTest {
     private final Map<String, BaseDriverController> driverControllerMap = new HashMap<>();
 
     @BeforeEach
-    public void setUp() throws MalformedURLException {
+    public void setUp() {
         logger = LoggerFactory.getLogger(SampleT2CTest.class);
         t2CJsonParser = new T2CJsonParser(logger);
         testInfo = t2CJsonParser.parseJsonFile(filePath);
 
-        service = AppiumDriverLocalService.buildService(new AppiumServiceBuilder()
-                .usingPort(4723).withArgument(GeneralServerFlag.BASEPATH, "/wd/hub/")
-                .withArgument(GeneralServerFlag.RELAXED_SECURITY)
-                .withArgument(GeneralServerFlag.LOG_LEVEL, "error")
-                .withArgument(GeneralServerFlag.ALLOW_INSECURE, "adb_shell"));
-        service.start();
-
+        try {
+            service = AppiumDriverLocalService.buildService(new AppiumServiceBuilder()
+                    .usingPort(4723).withArgument(GeneralServerFlag.BASEPATH, "/wd/hub/")
+                    .withArgument(GeneralServerFlag.RELAXED_SECURITY)
+                    .withArgument(GeneralServerFlag.LOG_LEVEL, "error")
+                    .withArgument(GeneralServerFlag.ALLOW_INSECURE, "adb_shell"));
+            service.start();
+        } catch (Exception e) {
+            service = null;
+            logger.info("Start Appium service failed will skip case!");
+        }
 
         getDriversMap(testInfo.getDrivers());
 
@@ -64,10 +65,13 @@ public class SampleT2CTest {
             caps.setCapability("noReset", true);
             if (driverInfo.getPlatform().equalsIgnoreCase("android")) {
                 caps.setCapability("automationName", "uiautomator2");
-                AndroidDriver androidDriver = new AndroidDriver(service.getUrl(), caps);
+                AndroidDriver androidDriver = null;
+                if (service != null) {
+                    androidDriver = new AndroidDriver(service.getUrl(), caps);
+                }
                 AndroidDriverController androidDriverController = new AndroidDriverController(androidDriver, logger);
                 driverControllerMap.put(driverInfo.getId(), androidDriverController);
-                if (driverInfo.getLauncherApp() != null && driverInfo.getLauncherApp().length() > 0) {
+                if (driverInfo.getLauncherApp() != null && driverInfo.getLauncherApp().length() > 0 && service != null) {
                     androidDriverController.activateApp(driverInfo.getLauncherApp());
                 }
             }
@@ -75,7 +79,10 @@ public class SampleT2CTest {
                 if (driverInfo.getLauncherApp() != null && driverInfo.getLauncherApp().length() > 0) {
                     caps.setCapability("app", driverInfo.getLauncherApp() + "!app");
                 }
-                WindowsDriver windowsDriver = new WindowsDriver(service.getUrl(), caps);
+                WindowsDriver windowsDriver = null;
+                if (service != null) {
+                    windowsDriver = new WindowsDriver(service.getUrl(), caps);
+                }
                 driverControllerMap.put(driverInfo.getId(), new WindowsDriverController(windowsDriver, logger));
             }
         }
@@ -88,7 +95,9 @@ public class SampleT2CTest {
         for (ActionInfo actionInfo : caseList) {
             BaseDriverController driverController = driverControllerMap.get(actionInfo.getDriverId());
             System.out.println(actionInfo.getDriverId());
-            T2CAppiumUtils.doAction(driverController, actionInfo);
+            if (driverController.webDriver != null) {
+                T2CAppiumUtils.doAction(driverController, actionInfo);
+            }
         }
     }
 
