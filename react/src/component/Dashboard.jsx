cc9df740c90544c858036e22e97396840b13e7d5
BryanSharp <8344245+hydraxman@users.noreply.github.com>
2023-02-04 08:42:20 +0800

    Add star button in the homepage list and clean up webpack config

--- react/src/component/Dashboard.jsx
+++ react/src/component/Dashboard.jsx
@@ -10,6 +10,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
+import Tooltip from "@mui/material/Tooltip";
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import DeviceAgentsView from '@/component/DeviceAgentsView'
@@ -246,16 +247,19 @@ export default function Dashboard() {
                                 </ListItemIcon>
                                 <ListItemText primary="Team Management" />
                             </ListItem>
-                            <ListItem component="a" href='https://microsoft.github.io/HydraLab/' target="_blank"
+                            <ListItem component="a" href='https://github.com/microsoft/HydraLab/blob/main/README.md' target="_blank"
                                       rel="noopener noreferrer" button>
                                 <ListItemIcon>
                                     <span className="material-icons-outlined">info</span>
                                 </ListItemIcon>
                                 <ListItemText primary="About"/>
                             </ListItem>
+                            <Tooltip title={"Star microsoft/HydraLab on GitHub"}>
+                                <iframe style={{float:'right',margin:'10px'}} src="https://ghbtns.com/github-btn.html?user=microsoft&repo=HydraLab&type=star&count=true" frameborder="0" scrolling="0" width="90" height="25" title="GitHub"></iframe>
+                            </Tooltip>
                             <ListItem
                                 style={{
-                                    height: "calc(100vh - 561px)",
+                                    height: "calc(100vh - 610px)",
                                     pointerEvents: "none"
                                 }}>
                             </ListItem>
