df6db849cb8cff7f09127e20989d3867e5d535ef
Le Zhou <2428499107@qq.com>
2023-02-17 15:56:04 +0800

    Bugfix: set the pc screenshot file to avoid NPE

--- common/src/main/java/com/microsoft/hydralab/common/util/ThreadPoolUtil.java
+++ common/src/main/java/com/microsoft/hydralab/common/util/ThreadPoolUtil.java
@@ -10,7 +10,7 @@ public class ThreadPoolUtil {
     private static final AtomicInteger SCREENSHOT_THREAD_NUMBER = new AtomicInteger(1);
     public static final Executor SCREENSHOT_EXECUTOR = newThreadPoolExecutor(SCREENSHOT_THREAD_NUMBER, 20, 60L, "ScreenshotExecutor");
     private static final AtomicInteger TEST_THREAD_NUMBER = new AtomicInteger(1);
-    public static final Executor TEST_EXECUTOR = newThreadPoolExecutor(TEST_THREAD_NUMBER, 30, 60L, "ScreenshotExecutor");
+    public static final Executor TEST_EXECUTOR = newThreadPoolExecutor(TEST_THREAD_NUMBER, 30, 60L, "TestExecutor");
 
     public static Executor newThreadPoolExecutor(AtomicInteger threadCounter, int corePoolSize, long keepAliveTimeSeconds, String threadNamePrefix) {
         return new ThreadPoolExecutor(corePoolSize, Integer.MAX_VALUE, keepAliveTimeSeconds,
