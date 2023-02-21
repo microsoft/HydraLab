a418b92ea7ab5743a6cf3aa5e32c56c3e0051a8d
Yuxing Hu <yuxihu@microsoft.com>
2023-02-21 17:58:59 +0800

    Update XctestRunner.java

--- agent/src/main/java/com/microsoft/hydralab/agent/runner/xctest/XctestRunner.java
+++ agent/src/main/java/com/microsoft/hydralab/agent/runner/xctest/XctestRunner.java
@@ -22,13 +22,15 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Map;
 import java.util.Objects;
 
 import cn.hutool.log.Log;
 
 public class XctestRunner extends TestRunner {
-    private static String filePath = "";
+    private static String zipPath = "";
     private static String folderPath = "";
+    private static String folderNamePath = "";
 
     public XctestRunner(DeviceManager deviceManager, TestTaskRunCallback testTaskRunCallback) {
         super(deviceManager, testTaskRunCallback);
@@ -37,50 +39,50 @@ public class XctestRunner extends TestRunner {
     @Override
     protected void run(DeviceInfo deviceInfo, TestTask testTask, TestRun testRun) throws Exception {
         Logger reportLogger = testRun.getLogger();
+        // Need a location here
         unzipXctestFolder("location", reportLogger);
         runXctest();
     }
 
     private void unzipXctestFolder(String location, Logger reportLogger) {
         File testBaseDir = new File(location);
-        String name = Const.SmartTestConfig.XCTEST_ZIP_FILE_NAME;
+        String zipName = Const.SmartTestConfig.XCTEST_ZIP_FILE_NAME;
+        String folderName = Const.SmartTestConfig.XCTEST_ZIP_FOLDER_NAME;
 
-        folderPath = testBaseDir.getAbsolutePath() + "/" + Const.SmartTestConfig.ZIP_FOLDER_NAME + "/";
-        stringFolderPath = testBaseDir.getAbsolutePath() + "/" + Const.SmartTestConfig.XCTEST_ZIP_FOLDER_NAME
+        zipPath = testBaseDir.getAbsolutePath() + "/" + Const.SmartTestConfig.ZIP_FOLDER_NAME + "/";
+        folderPath = testBaseDir.getAbsolutePath() + "/" + Const.SmartTestConfig.XCTEST_ZIP_FOLDER_NAME
                 + "/";
 
-
         try {
-            reportLogger.info("Start instrumenting the test");
-            InputStream resourceAsStream = FileUtils.class.getClassLoader().getResourceAsStream(name);
+            reportLogger.info("Start install xctest files");
+            InputStream resourceAsStream = FileUtils.class.getClassLoader().getResourceAsStream(zipName);
             if (resourceAsStream == null) {
                 return;
             }
-            File xctestZip = new File(testBaseDir, name);
-            File smartTestFolder = new File(testBaseDir, folderName);
+            File xctestZip = new File(testBaseDir, zipName);
+            File xctestFolder = new File(testBaseDir, folderName);
             if (xctestZip.exists()) {
                 FileUtil.deleteFileRecursively(xctestZip);
             }
-            if (smartTestFolder.exists()) {
-                FileUtil.deleteFileRecursively(smartTestFolder);
+            if (xctestFolder.exists()) {
+                FileUtil.deleteFileRecursively(xctestFolder);
             }
             OutputStream out = new FileOutputStream(xctestZip);
             IOUtils.copy(Objects.requireNonNull(resourceAsStream), out);
             out.close();
             FileUtil.unzipFile(xctestZip.getAbsolutePath(), testBaseDir.getAbsolutePath());
-            if (smartTestZip.exists()) {
-                FileUtil.deleteFileRecursively(smartTestZip);
+            if (xctestZip.exists()) {
+                FileUtil.deleteFileRecursively(xctestZip);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
     }
 
-    private String runXctest(DeviceInfo deviceInfo, String scope, String suiteName, String testPkgName, String testRunnerName, Logger logger, IShellOutputReceiver
-            receiver,
-                           int testTimeOutSec, Map<String, String> instrumentationArgs) {
+    private String runXctest(DeviceInfo deviceInfo, String scope, String suiteName,
+                             String testPkgName, String testRunnerName, Logger logger,
+                             IShellOutputReceiver receiver,
+                             int testTimeOutSec, Map<String, String> instrumentationArgs) {
         if (deviceInfo == null) {
             throw new RuntimeException("No such device: " + deviceInfo);
         }
@@ -94,35 +96,19 @@ public class XctestRunner extends TestRunner {
         }
         String commFormat;
         if (StringUtils.isBlank(argString.toString())) {
-            commFormat = "am instrument -w -r -e debug false";
+            commFormat = "xcodebuild test-without-building";
         } else {
-            commFormat = "am instrument -w -r" + argString + " -e debug false";
+            commFormat = "xcodebuild test-without-building" + argString;
         }
 
         try {
-            String command;
-            switch (scope) {
-                case TestTask.TestScope.TEST_APP:
-                    commFormat += " %s/%s";
-                    command = String.format(commFormat, testPkgName, testRunnerName);
-                    break;
-                case TestTask.TestScope.PACKAGE:
-                    commFormat += " -e package %s %s/%s";
-                    command = String.format(commFormat, suiteName, testPkgName, testRunnerName);
-                    break;
-                // Const.TestScope.CLASS
-                default:
-                    commFormat += " -e class %s %s/%s";
-                    command = String.format(commFormat, suiteName, testPkgName, testRunnerName);
-                    break;
-            }
+            String destination = "name=iPhone 14 Pro";
+            String resultPath = folderPath + "/" + "result";
 
-            if (logger != null) {
-                // make sure pass is not printed
-                logger.info(">> adb -s {} shell {}", deviceInfo.getSerialNum(), LogUtils.scrubSensitiveArgs(command));
-            }
-            String result = ShellUtils.execLocalCommandWithResult(
-                    "xcodebuild test-without-building -destination " + destination + " -resultBundlePath ", logger);
+            commFormat += " -destination %s -resultBundlePath %s";
+            String command = String.format(commFormat, destination, resultPath);
+
+            String result = ShellUtils.execLocalCommandWithResult(command, logger);
             return Const.TaskResult.SUCCESS;
         } catch (Exception e) {
             if (logger != null) {
