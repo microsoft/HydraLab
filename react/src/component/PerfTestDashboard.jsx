d576064e5f0505e6bcd421c424d63430a7aa7562
Ran Tao <1252026067@qq.com>
2023-03-10 15:20:16 +0800

    Add multi-select for perf dashboard

--- react/src/component/PerfTestDashboard.jsx
+++ react/src/component/PerfTestDashboard.jsx
@@ -1,77 +1,52 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import Select, { MultiValue } from 'react-select'
+import Select from 'react-select'
 import makeAnimated from 'react-select/animated';
-import React, { PureComponent } from 'react'
+import React from 'react'
 import axios from "@/axios";
 import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend } from 'recharts';
 
-const COLORS = ['#007FFF', '#FFA500', '#8B8970', '#800000', '#FFCC00', '#808000', '#4B0080', '#8884d8', '#7EC0EE',
-    '#8B008B', '#EE7600', '#CD5C5C', '#BC8F8F', '#8B8B7A', '#006400', '#FF69B4', '#90EE90', '#A4D3EE',
-    '#8884d8', '#8884d8', '#8884d8', '#8884d8', '#8884d8', '#8884d8', '#8884d8', '#8884d8', '#8884d8'];
 const animatedComponents = makeAnimated();
 const androidBatteryOptions = [
-    { value: 'appUsage', label: 'appUsage' },
-    { value: 'ratio', label: 'ratio' },
-    { value: 'cpu', label: 'cpu' },
-    { value: 'systemService', label: 'systemService' },
-    { value: 'screen', label: 'screen' },
-    { value: 'wakelock', label: 'wakelock' },
-    { value: 'wifi', label: 'wifi' },
+    { value: 'appUsage', label: 'appUsage', color: '#007FFF' },
+    { value: 'ratio', label: 'ratio', color: '#FFA500' },
+    { value: 'cpu', label: 'cpu', color: '#8B8970' },
+    { value: 'systemService', label: 'systemService', color: '#800000' },
+    { value: 'screen', label: 'screen', color: '#FFCC00' },
+    { value: 'wakelock', label: 'wakelock', color: '#808000' },
+    { value: 'wifi', label: 'wifi', color: '4B0080' },
     // { value: 'total', label: 'total' },
 ]
 const androidMemoryOptions = [
-    { value: 'javaHeapPss', label: 'javaHeapPss' },
-    { value: 'nativeHeapPss', label: 'nativeHeapPss' },
-    { value: 'codePss', label: 'codePss' },
-    { value: 'stackPss', label: 'stackPss' },
-    { value: 'graphicsPss', label: 'graphicsPss' },
-    { value: 'privateOtherPss', label: 'privateOtherPss' },
-    { value: 'systemPss', label: 'systemPss' },
-    { value: 'totalPss', label: 'totalPss' },
-    { value: 'totalRss', label: 'totalRss' },
-    { value: 'totalSwapPss', label: 'totalSwapPss' },
-    { value: 'javaHeapRss', label: 'javaHeapRss' },
-    { value: 'nativeHeapRss', label: 'nativeHeapRss' },
-    { value: 'codeRss', label: 'codeRss' },
-    { value: 'stackRss', label: 'stackRss' },
-    { value: 'graphicsRss', label: 'graphicsRss' },
-    { value: 'privateOtherRss', label: 'privateOtherRss' },
-    { value: 'systemRss', label: 'systemRss' },
-    { value: 'unknownPss', label: 'unknownPss' },
-    { value: 'unknownRss', label: 'unknownRss' }
+    { value: 'javaHeapPss', label: 'javaHeapPss', color: '#007FFF' },
+    { value: 'nativeHeapPss', label: 'nativeHeapPss', color: '#FFA500' },
+    { value: 'codePss', label: 'codePss', color: '#8B8970' },
+    { value: 'stackPss', label: 'stackPss', color: '#800000' },
+    { value: 'graphicsPss', label: 'graphicsPss', color: '#FFCC00' },
+    { value: 'privateOtherPss', label: 'privateOtherPss', color: '#808000' },
+    { value: 'systemPss', label: 'systemPss', color: '#4B0080' },
+    { value: 'totalPss', label: 'totalPss', color: '#8884d8' },
+    { value: 'totalRss', label: 'totalRss', color: '#8B008B' },
+    { value: 'totalSwapPss', label: 'totalSwapPss', color: '#EE7600' },
+    { value: 'javaHeapRss', label: 'javaHeapRss', color: '#CD5C5C' },
+    { value: 'nativeHeapRss', label: 'nativeHeapRss', color: '#BC8F8F' },
+    { value: 'codeRss', label: 'codeRss', color: '#8B8B7A' },
+    { value: 'stackRss', label: 'stackRss', color: '#006400' },
+    { value: 'graphicsRss', label: 'graphicsRss', color: '#FF69B4' },
+    { value: 'privateOtherRss', label: 'privateOtherRss', color: '#90EE90' },
+    { value: 'systemRss', label: 'systemRss', color: '#A4D3EE' },
+    { value: 'unknownPss', label: 'unknownPss', color: '#8884d8' },
+    { value: 'unknownRss', label: 'unknownRss', color: '#8884d8' }
 ]
-
-const memoryKey = [
-    'javaHeapPss',
-    // 'javaHeapRss',
-    // 'nativeHeapRss',
-    'nativeHeapPss',
-    'codePss',
-    // 'codeRss',
-    'stackPss',
-    // 'stackRss',
-    'graphicsPss',
-    // 'graphicsRss',
-    'privateOtherPss',
-    // 'privateOtherRss',
-    'systemPss',
-    // 'systemRss',
-    // 'unknownPss',
-    // 'unknownRss',
-    'totalPss',
-    'totalRss',
-    'totalSwapPss'
-];
 
 export default class PerfTestDashboard extends React.Component {
     state = {
         perfTestResult: this.props.perfTestResult,
         memoryInfo: undefined,
         batteryInfo: undefined,
-        selectedAndroidBatteryOptions: androidBatteryOptions,
-        selectedAndroidMemoryOptions: androidMemoryOptions,
+        selectedAndroidBatteryOptions: androidBatteryOptions.slice(0, 4),
+        selectedAndroidMemoryOptions: androidMemoryOptions.slice(0, 10),
     };
 
     render() {
@@ -95,9 +70,9 @@ export default class PerfTestDashboard extends React.Component {
             })
         }
 
-        const batteryMultiSelect = (
+        const androidBatteryMultiSelect = (
             <Select
-                defaultValue={[androidBatteryOptions[0], androidBatteryOptions[1], androidBatteryOptions[2], androidBatteryOptions[3]]}
+                defaultValue={androidBatteryOptions.slice(0, 4)}
                 isMulti
                 components={animatedComponents}
                 options={androidBatteryOptions}
@@ -107,13 +82,13 @@ export default class PerfTestDashboard extends React.Component {
             />
         );
 
-        const renderBatteryChart = (
+        const renderAndroidBatteryChart = (
             <LineChart width={800} height={400} data={batteryMetrics} margin={{ top: 20, right: 100, bottom: 20, left: 20 }}>
                 <XAxis dataKey="time" label={{ value: 'Time', position: 'bottom' }} unit="s" />
                 <YAxis yAxisId="left" label={{ value: 'Battery usage (mAh)', angle: -90, position: 'left' }} />
                 <YAxis yAxisId="right" label={{ value: 'Ratio', angle: -90, position: 'right' }} unit="%" orientation="right" />
                 {this.state.selectedAndroidBatteryOptions.map((key, index) => (
-                    <Line type="monotone" yAxisId={key.value == "ratio" ? "right" : "left"} dataKey={key.value} stroke={COLORS[index]} />
+                    <Line type="monotone" yAxisId={key.value == "ratio" ? "right" : "left"} dataKey={key.value} stroke={key.color} />
                 ))}
                 {/* <CartesianGrid stroke="#ccc" strokeDasharray="5 5" /> */}
                 <Tooltip />
@@ -133,7 +108,7 @@ export default class PerfTestDashboard extends React.Component {
                     Object.keys(inspectionResult.parsedData).forEach((key) => {
                         if (inspectionResult.parsedData[key] == -1) {
                             result[key] = 0;
-                        } else if (memoryKey.indexOf(key) > -1) {
+                        } else if (androidMemoryOptions.findIndex(item => item.value == key) != -1) {
                             // KB to MB 
                             result[key] = inspectionResult.parsedData[key] / 1024;
                         }
@@ -143,9 +118,9 @@ export default class PerfTestDashboard extends React.Component {
             })
         }
 
-        const memoryMultiSelect = (
+        const androidMemoryMultiSelect = (
             <Select
-                defaultValue={[androidMemoryOptions[0], androidMemoryOptions[1], androidMemoryOptions[2], androidMemoryOptions[3]]}
+                defaultValue={androidMemoryOptions.slice(0, 10)}
                 isMulti
                 components={animatedComponents}
                 options={androidMemoryOptions}
@@ -155,13 +130,13 @@ export default class PerfTestDashboard extends React.Component {
             />
         );
 
-        const renderMemoryChart = (
+        const renderAndroidMemoryChart = (
             <LineChart width={800} height={400} data={memoryMetrics} margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
                 <Legend verticalAlign="top" />
                 <XAxis dataKey="time" label={{ value: 'Time', position: 'bottom' }} unit="s" />
                 <YAxis yAxisId="left" label={{ value: 'Memory usage (MB)', angle: -90, position: 'left' }} />
                 {this.state.selectedAndroidMemoryOptions.map((key, index) => (
-                    <Line type="monotone" yAxisId="left" dataKey={key.value} stroke={COLORS[index]} />
+                    <Line type="monotone" yAxisId="left" dataKey={key.value} stroke={key.color} />
                 ))}
                 {/* <CartesianGrid stroke="#ccc" strokeDasharray="5 5" /> */}
                 <Tooltip />
@@ -169,11 +144,16 @@ export default class PerfTestDashboard extends React.Component {
             </LineChart>)
 
         return <div id='perf_dashboard'>
-            {batteryInfo && <h3> Battery report</h3>}
-            {batteryInfo && batteryMultiSelect}
-            {batteryInfo && renderBatteryChart}
-            {memoryInfo && <h3> Memory report</h3>}
-            {memoryInfo && renderMemoryChart}
+            {batteryInfo && <div>
+                <h3> Battery report</h3>
+                {androidBatteryMultiSelect}
+                {renderAndroidBatteryChart}
+            </div>}
+            {memoryInfo && <div>
+                <h3> Memory report</h3>
+                {androidMemoryMultiSelect}
+                {renderAndroidMemoryChart}
+            </div>}
         </div>
     };
 
