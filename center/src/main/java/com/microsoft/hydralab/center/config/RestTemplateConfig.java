953c82fd4f192b2a1d8659e8546e82fdff9d7664
Li Shen <olivershen1994@gmail.com>
2023-03-14 16:04:04 +0800

    check style

--- center/src/main/java/com/microsoft/hydralab/center/config/RestTemplateConfig.java
+++ center/src/main/java/com/microsoft/hydralab/center/config/RestTemplateConfig.java
@@ -10,11 +10,7 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.ssl.SSLContexts;
 import org.apache.http.ssl.TrustStrategy;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.web.client.RestTemplate;
 
 import javax.net.ssl.SSLContext;
 import java.security.KeyManagementException;
