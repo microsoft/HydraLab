aea1499275c76144bfc49811bb75bcd5fc4cbd01
OliverShen <41765758+olivershen-wow@users.noreply.github.com>
2023-03-14 16:39:54 +0800

    Remove configuration annotation that creates useless beans (#367)
    * Remove configuration annotation that creates useless beans

--- common/src/main/java/com/microsoft/hydralab/common/util/RestTemplateConfig.java
+++ common/src/main/java/com/microsoft/hydralab/common/util/RestTemplateConfig.java
@@ -1,8 +1,9 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-package com.microsoft.hydralab.center.config;
+package com.microsoft.hydralab.common.util;
 
+import cn.hutool.http.HttpStatus;
 import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -10,9 +11,6 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.ssl.SSLContexts;
 import org.apache.http.ssl.TrustStrategy;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
@@ -21,7 +19,6 @@ import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 
-@Configuration
 public class RestTemplateConfig {
 
     public static HttpComponentsClientHttpRequestFactory generateHttpRequestFactory()
@@ -38,16 +35,14 @@ public class RestTemplateConfig {
         return factory;
     }
 
-    @Bean
-    public RestTemplate restTemplate(ClientHttpRequestFactory factory) {
-        return new RestTemplate(factory);
-    }
+    public static RestTemplate getRestTemplateInstance() {
+        RestTemplate restTemplateHttps;
+        try {
+            restTemplateHttps = new RestTemplate(generateHttpRequestFactory());
+        } catch (NoSuchAlgorithmException | KeyManagementException | KeyStoreException e) {
+            throw new HydraLabRuntimeException(HttpStatus.HTTP_INTERNAL_ERROR, "Failed to create SSLContext for RestTemplate.");
+        }
 
-    @Bean
-    public ClientHttpRequestFactory simpleClientHttpRequestFactory() {
-        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
-        factory.setConnectTimeout(15000);
-        factory.setReadTimeout(5000);
-        return factory;
+        return restTemplateHttps;
     }
 }
