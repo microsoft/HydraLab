a2ea71d734cd5286b89beaf46d1d2dffaabcbce1
Li Shen <olivershen1994@gmail.com>
2023-03-14 16:18:48 +0800

    Update

--- common/src/main/java/com/microsoft/hydralab/common/util/RestTemplateConfig.java
+++ common/src/main/java/com/microsoft/hydralab/common/util/RestTemplateConfig.java
@@ -1,8 +1,9 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-package com.microsoft.hydralab.center.config;
+package com.microsoft.hydralab.common.util;
 
+import cn.hutool.http.HttpStatus;
 import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -11,6 +12,7 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.ssl.SSLContexts;
 import org.apache.http.ssl.TrustStrategy;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 
 import javax.net.ssl.SSLContext;
 import java.security.KeyManagementException;
@@ -31,5 +33,16 @@ public class RestTemplateConfig {
         HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
         factory.setHttpClient(httpClient);
         return factory;
+    }
+
+    public static RestTemplate getRestTemplateInstance() {
+        RestTemplate restTemplateHttps;
+        try {
+            restTemplateHttps = new RestTemplate(generateHttpRequestFactory());
+        } catch (NoSuchAlgorithmException | KeyManagementException | KeyStoreException e) {
+            throw new HydraLabRuntimeException(HttpStatus.HTTP_INTERNAL_ERROR, "Failed to create SSLContext for RestTemplate.");
+        }
+
+        return restTemplateHttps;
     }
 }
