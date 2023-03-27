3cd07ea5df4f5fda50273d75321d17611d395536
Li Shen <olivershen1994@gmail.com>
2023-03-27 18:08:55 +0800

    Update with latest investigation result

--- agent/start.sh
+++ agent/start.sh
@@ -1,6 +1,6 @@
 adb start-server
 # start emulator
 # The binary emulator-headless is now retired. Headless builds of the engine are now launched via emulator -no-window, thus unifying the previously separate (but similar) paths.
-nohup emulator -avd emuTestNexus -noaudio -no-boot-anim -gpu off -no-window -qemu -machine virt &
+nohup emulator -avd emuTestPixel -noaudio -no-boot-anim -gpu off -no-window -qemu -machine virt &
 
 java -jar /app.jar --spring.profiles.active=docker
