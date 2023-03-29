0795db4cc88778c26f0513272b6cf0ecc3325b3e
Millard Ding <dinghong2233@126.com>
2023-03-29 12:31:22 +0800

    Update common/src/main/java/com/microsoft/hydralab/common/management/device/impl/IOSTestDeviceManager.java

--- common/src/main/java/com/microsoft/hydralab/common/management/device/impl/IOSTestDeviceManager.java
+++ common/src/main/java/com/microsoft/hydralab/common/management/device/impl/IOSTestDeviceManager.java
@@ -31,7 +31,6 @@ import org.slf4j.LoggerFactory;
 import java.io.File;
 import java.io.IOException;
 import java.time.Duration;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
