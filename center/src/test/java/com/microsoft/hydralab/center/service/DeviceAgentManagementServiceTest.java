6cff313c2f58b44bd48ce28c560cc06ef7f02349
BryanSharp <370788475@qq.com>
2023-04-06 19:10:29 +0800

    Update UT

--- center/src/test/java/com/microsoft/hydralab/center/service/DeviceAgentManagementServiceTest.java
+++ center/src/test/java/com/microsoft/hydralab/center/service/DeviceAgentManagementServiceTest.java
@@ -4,14 +4,16 @@ import com.microsoft.hydralab.center.repository.AgentUserRepository;
 import com.microsoft.hydralab.center.test.BaseTest;
 import com.microsoft.hydralab.common.entity.common.AgentUser;
 import com.microsoft.hydralab.common.entity.common.Message;
+import com.microsoft.hydralab.common.file.StorageServiceClientProxy;
 import com.microsoft.hydralab.common.util.Const;
 import com.microsoft.hydralab.common.util.SerializeUtil;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.ApplicationContext;
 
 import javax.annotation.Resource;
+import javax.websocket.RemoteEndpoint;
 import javax.websocket.Session;
 import java.io.IOException;
 import java.net.InetAddress;
@@ -24,11 +26,16 @@ public class DeviceAgentManagementServiceTest extends BaseTest {
     DeviceAgentManagementService deviceAgentManagementService;
     @MockBean
     AgentUserRepository agentUserRepository;
+    @Resource
+    ApplicationContext applicationContext;
+
 
     @Test
     public void testOnMessage() throws IOException {
         Session session = Mockito.mock(Session.class);
+        RemoteEndpoint.Basic basicRemote = Mockito.mock(RemoteEndpoint.Basic.class);
         Mockito.when(session.getId()).thenReturn("123456");
+        Mockito.when(session.getBasicRemote()).thenReturn(basicRemote);
 
         String agentId = "agentId";
         AgentUser agentUser = new AgentUser();
@@ -45,14 +52,14 @@ public class DeviceAgentManagementServiceTest extends BaseTest {
 
         given(agentUserRepository.findById(agentId)).willReturn(Optional.of(agentUser));
 
+        StorageServiceClientProxy storageServiceClientProxy = new StorageServiceClientProxy(applicationContext);
+        storageServiceClientProxy.initCenterStorageClient("LOCAL");
+        given(this.storageServiceClientProxy.generateAccessToken(Const.FilePermission.WRITE)).willReturn(storageServiceClientProxy.generateAccessToken(Const.FilePermission.WRITE));
+
         Message message = Message.ok(Const.Path.AUTH, agentUser);
         byte[] byteMsg = SerializeUtil.messageToByteArr(message);
         // will throw exception when invoking: com.microsoft.hydralab.center.service.StorageTokenManageService.generateWriteToken
-        Assertions.assertThrows(IllegalArgumentException.class,
-                () -> deviceAgentManagementService.onMessage(SerializeUtil.byteArrToMessage(byteMsg), session),
-                "Current storage service doesn't config WRITE permission!");
-
+        deviceAgentManagementService.onMessage(SerializeUtil.byteArrToMessage(byteMsg), session);
         session.close();
-
     }
 }
