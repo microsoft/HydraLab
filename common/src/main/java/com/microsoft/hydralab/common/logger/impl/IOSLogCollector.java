4504b77e6973a612f46d287d0e3acb7cd9cfc565
Le Zhou <2428499107@qq.com>
2023-04-19 09:37:17 +0800

    Update common/src/main/java/com/microsoft/hydralab/common/logger/impl/IOSLogCollector.java
    Co-authored-by: Millard Ding <dinghong2233@126.com>

--- common/src/main/java/com/microsoft/hydralab/common/logger/impl/IOSLogCollector.java
+++ common/src/main/java/com/microsoft/hydralab/common/logger/impl/IOSLogCollector.java
@@ -82,6 +82,7 @@ public class IOSLogCollector implements LogCollector {
                 }
             }
             if (crashLines.length() > 0) {
+// TODO (Millard): Try to analysis the crash log to shorten the crash log
                 if (crashLines.length() > 2000) {
                     crashLines = new StringBuilder(crashLines.substring(0, 2000));
                 }
