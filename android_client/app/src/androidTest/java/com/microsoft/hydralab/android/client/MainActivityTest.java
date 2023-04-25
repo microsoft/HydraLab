d601bf91e78db5a8fa145cf49dd1d23d4ed01b95
Li Shen <olivershen1994@gmail.com>
2023-04-25 18:46:32 +0800

    Update android_client test

--- android_client/app/src/androidTest/java/com/microsoft/hydralab/android/client/MainActivityTest.java
+++ android_client/app/src/androidTest/java/com/microsoft/hydralab/android/client/MainActivityTest.java
@@ -1,14 +1,23 @@
 package com.microsoft.hydralab.android.client;
 
 
-import android.support.test.espresso.ViewInteraction;
-import android.support.test.filters.LargeTest;
-import android.support.test.rule.ActivityTestRule;
-import android.support.test.rule.GrantPermissionRule;
-import android.support.test.runner.AndroidJUnit4;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.filters.LargeTest;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.rule.GrantPermissionRule;
+import androidx.test.runner.AndroidJUnit4;
+import androidx.test.uiautomator.UiDevice;
+import androidx.test.uiautomator.UiObject;
+import androidx.test.uiautomator.UiObjectNotFoundException;
+import androidx.test.uiautomator.UiSelector;
+
+import android.app.UiAutomation;
+import android.os.ParcelFileDescriptor;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
+import android.widget.FrameLayout;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
@@ -18,15 +27,18 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withParent;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
+
+import java.io.BufferedReader;
+import java.io.IOException;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
@@ -42,7 +54,21 @@ public class MainActivityTest {
                     "android.permission.WRITE_EXTERNAL_STORAGE");
 
     @Test
-    public void mainActivityTest() {
+    public void mainActivityTest() throws UiObjectNotFoundException, InterruptedException, IOException {
+        UiDevice device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+        UiObject uiObject = device.findObject(new UiSelector().packageName("com.android.systemui").textMatches("(?i)(START NOW|ALLOW|允许)"));
+        if (uiObject.exists()) {
+            uiObject.click();
+        }
+
+        ViewInteraction button = onView(
+                allOf(withId(R.id.record_button), withText("START RECORDER"),
+                        withParent(withParent(withId(R.id.container))),
+                        isDisplayed()));
+        button.check(matches(isDisplayed()));
+        button.perform(click());
+        Thread.sleep(1000);
+
         ViewInteraction imageView = onView(
                 allOf(withId(R.id.debug_icon), withContentDescription("Debug"),
                         childAtPosition(
@@ -65,8 +91,8 @@ public class MainActivityTest {
 
         ViewInteraction textView = onView(
                 allOf(withText("HydraLab Client"),
-                        withParent(allOf(IsInstanceOf.<View>instanceOf(android.view.ViewGroup.class),
-                                withParent(IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class)))),
+                        withParent(allOf(IsInstanceOf.<View>instanceOf(ViewGroup.class),
+                                withParent(IsInstanceOf.<View>instanceOf(FrameLayout.class)))),
                         isDisplayed()));
         textView.check(matches(withText("HydraLab Client")));
 
@@ -83,12 +109,6 @@ public class MainActivityTest {
                         isDisplayed()));
         imageView4.check(matches(isDisplayed()));
 
-        ViewInteraction button = onView(
-                allOf(withId(R.id.record_button), withText("START RECORDER"),
-                        withParent(withParent(withId(R.id.container))),
-                        isDisplayed()));
-        button.check(matches(isDisplayed()));
-
         ViewInteraction imageView5 = onView(
                 allOf(withId(R.id.debug_icon), withContentDescription("Debug"),
                         childAtPosition(
@@ -98,6 +118,14 @@ public class MainActivityTest {
                                 0),
                         isDisplayed()));
         imageView5.perform(click());
+
+        ViewInteraction button2 = onView(
+                allOf(withId(R.id.record_button), withText("STOP RECORDER"),
+                        withParent(withParent(withId(R.id.container))),
+                        isDisplayed()));
+        button2.check(matches(isDisplayed()));
+        button2.perform(click());
+        Thread.sleep(1000);
     }
 
     private static Matcher<View> childAtPosition(
