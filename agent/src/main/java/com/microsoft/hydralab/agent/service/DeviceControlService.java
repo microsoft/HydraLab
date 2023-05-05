f5e5a3893b2b1e075a14509e0f7ed92f2fa117b2
Le Zhou <2428499107@qq.com>
2023-05-05 15:45:40 +0800

    Update DeviceControlService.java

--- agent/src/main/java/com/microsoft/hydralab/agent/service/DeviceControlService.java
+++ agent/src/main/java/com/microsoft/hydralab/agent/service/DeviceControlService.java
@@ -69,9 +69,9 @@ public class DeviceControlService {
         // we need to do this in an async way, otherwise the process will be blocked if one device is not responding
         allConnectedDevices.forEach(deviceInfo -> {
             ThreadPoolUtil.SCREENSHOT_EXECUTOR.execute(() -> {
-                try{
+                try {
                     deviceDriverManager.getScreenShotWithStrategy(deviceInfo, log, batteryStrategy);
-                }catch (Exception e){
+                } catch (Exception e) {
                     log.error("Failed to capture screenshot for device: {}", deviceInfo.getSerialNum(), e);
                 }
             });
