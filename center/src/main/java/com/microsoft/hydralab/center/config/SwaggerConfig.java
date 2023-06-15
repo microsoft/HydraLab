d27e69929003add904760c5aa50dc83f030022f8
OliverShen <41765758+olivershen-wow@users.noreply.github.com>
2023-06-15 11:05:00 +0800

    Upgrade swagger from version 2 to 3; (#496)
    Alter related dependency libs and configurations;
    Disable default swagger page with sample data;

--- center/src/main/java/com/microsoft/hydralab/center/config/SwaggerConfig.java
+++ center/src/main/java/com/microsoft/hydralab/center/config/SwaggerConfig.java
@@ -3,47 +3,19 @@
 
 package com.microsoft.hydralab.center.config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 
 /**
  * Created by bsp on 17/12/16.
  */
 @Configuration
-@EnableSwagger2
-//@Profile({"dev", "test"})
+@OpenAPIDefinition(info =
+@Info(title = "Hydra Lab API", version = "1.0.0", description = "Hydra Lab", contact =
+@Contact(name = "Microsoft Hydra Lab Support", email = "hydra_lab_support@microsoft.com")
+)
+)
 public class SwaggerConfig {
-    @Value("${server.port}")
-    private String port;
-
-    @Bean
-    public Docket docket() throws UnknownHostException {
-        ApiInfo apiInfo = new ApiInfoBuilder()
-                .title("Hydra Lab API")
-                .description("Hydra Lab")
-                .termsOfServiceUrl("")
-                .contact(new Contact("Microsoft Hydra Lab Support ", "", "hydra_lab_support@microsoft.com"))
-                .version("3.0.0")
-                .build();
-
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo)
-                .host(String.format("%s:%s", InetAddress.getLocalHost().getHostName(), port))
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.microsoft.hydralab.center.controller"))
-                .paths(PathSelectors.any())
-                .build();
-    }
 }
