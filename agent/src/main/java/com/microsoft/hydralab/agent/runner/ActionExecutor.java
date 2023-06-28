fd1c2326f678892ca94836fd8b9427ee7c76f96a
Li Shen <olivershen1994@gmail.com>
2023-06-28 10:24:19 +0800

    Fix executor

--- agent/src/main/java/com/microsoft/hydralab/agent/runner/ActionExecutor.java
+++ agent/src/main/java/com/microsoft/hydralab/agent/runner/ActionExecutor.java
@@ -53,7 +53,7 @@ public class ActionExecutor {
         if (runOnAgentOnly) {
             legalActionStream = legalActionStream.filter(deviceAction -> Const.OperatedDevice.AGENT.equalsIgnoreCase(deviceAction.getDeviceType()));
         } else {
-            legalActionStream = legalActionStream.filter(deviceAction -> testRunDevice.getDeviceInfo().getType().equalsIgnoreCase(deviceAction.getDeviceType()));
+            legalActionStream = legalActionStream.filter(deviceAction -> StringUtils.isEmpty(deviceAction.getDeviceType()) || testRunDevice.getDeviceInfo().getType().equalsIgnoreCase(deviceAction.getDeviceType()));
         }
         List<DeviceAction> todoActions = legalActionStream.collect(Collectors.toList());
 
