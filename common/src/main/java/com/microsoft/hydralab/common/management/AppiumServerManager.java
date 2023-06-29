0887a694b548d0a398fde8cccdaea26de42eff5e
Le Zhou <2428499107@qq.com>
2023-06-29 11:12:10 +0800

    Bugfix: unlockDevice method would make the phone back to desktop (#519)
    * Bugfix: unlockDevice method would make the phone back to desktop when testing
    
    * add some comments
    
    * Update IOSDeviceDriver.java

--- common/src/main/java/com/microsoft/hydralab/common/management/AppiumServerManager.java
+++ common/src/main/java/com/microsoft/hydralab/common/management/AppiumServerManager.java
@@ -287,7 +287,9 @@ public class AppiumServerManager {
 
     public Boolean isDriverAlive(IOSDriver driver) {
         try {
-            driver.unlockDevice();
+            // use getBatteryInfo to check if driver is alive
+            // getStatus would return from cache when session was closed and can't check if driver is alive
+            driver.getBatteryInfo();
             return true;
         } catch (WebDriverException e) {
             return false;
