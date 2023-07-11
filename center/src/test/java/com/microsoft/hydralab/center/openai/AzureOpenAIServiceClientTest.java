aee57d73cd50197e81a5028baf3f0020569ee24f
Nathan Bu <370788475@qq.com>
2023-07-11 11:43:41 +0800

    Provide new UT and refactor to extract the method (#529)

--- center/src/test/java/com/microsoft/hydralab/center/openai/AzureOpenAIServiceClientTest.java
+++ center/src/test/java/com/microsoft/hydralab/center/openai/AzureOpenAIServiceClientTest.java
@@ -1,11 +1,25 @@
 package com.microsoft.hydralab.center.openai;
 
 import com.microsoft.hydralab.center.test.BaseTest;
+import okhttp3.Call;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Protocol;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.platform.commons.util.StringUtils;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Value;
 
+import java.io.IOException;
 import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
@@ -18,13 +32,51 @@ public class AzureOpenAIServiceClientTest extends BaseTest {
     private String deployment;
     @Value("${app.openai.chat-completion.api-version:}")
     private String apiVersion;
+    private AzureOpenAIServiceClient azureOpenAIServiceClient;
+
+    @BeforeEach
+    public void setUp() {
+        if (StringUtils.isBlank(endpoint)) {
+            endpoint = "https://your-azure-openai-endpoint.openai.azure.com/";
+        }
+
+        azureOpenAIServiceClient =
+                new AzureOpenAIServiceClient(apiKey, deployment, endpoint, apiVersion);
+    }
 
     @Test
-    public void createAzureOpenAIServiceClientAndAsk() {
+    public void testChatCompletionWithMock() throws IOException {
+        OkHttpClient client = Mockito.mock(OkHttpClient.class);
+        Call mockCall = Mockito.mock(Call.class);
+
+        Response response = new Response.Builder()
+                .protocol(Protocol.HTTP_1_1).message("testResponse")
+                .request(new Request.Builder().url("https://mock.com").build())
+                .code(200)
+                .body(ResponseBody
+                        .create("testResponse", MediaType.parse("application/json"))
+                )
+                .build();
+        when(client.newCall(any(Request.class))).thenReturn(mockCall);
+        when(mockCall.execute()).thenReturn(response);
+
+        ChatRequest request = new ChatRequest();
+        request.setMessages(Arrays.asList(
+                new ChatMessage(ChatMessage.Role.SYSTEM, "You are an AI assistant that helps people find information."),
+                new ChatMessage(ChatMessage.Role.USER, "Could you tell me a joke?")
+        ));
+
+        azureOpenAIServiceClient.client = client;
+
+        String result = azureOpenAIServiceClient.chatCompletion(request);
+        assertEquals("testResponse", result);
+    }
+
+    @Test
+    public void testAzureOpenAIServiceClientChatCompletion() {
         if (StringUtils.isBlank(apiKey)) {
             return;
         }
-        AzureOpenAIServiceClient azureOpenAIServiceClient = new AzureOpenAIServiceClient(apiKey, deployment, endpoint, apiVersion);
         ChatRequest request = new ChatRequest();
         request.setMessages(Arrays.asList(
                 new ChatMessage(ChatMessage.Role.SYSTEM, "You are an AI assistant that helps people find information."),
@@ -32,4 +84,5 @@ public class AzureOpenAIServiceClientTest extends BaseTest {
         ));
         baseLogger.info(azureOpenAIServiceClient.chatCompletion(request));
     }
+
 }
