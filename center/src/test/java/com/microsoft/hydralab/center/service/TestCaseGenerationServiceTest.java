ddf93f04392a298c51fd7e5bb3e37e8161ddaa8b
Le Zhou <2428499107@qq.com>
2023-07-24 15:31:22 +0800

    rebuild case generation code

--- center/src/test/java/com/microsoft/hydralab/center/service/TestCaseGenerationServiceTest.java
+++ center/src/test/java/com/microsoft/hydralab/center/service/TestCaseGenerationServiceTest.java
@@ -1,5 +1,6 @@
 package com.microsoft.hydralab.center.service;
 
+import com.microsoft.hydralab.center.service.generation.MaestroCaseGenerationService;
 import com.microsoft.hydralab.center.test.BaseTest;
 import com.microsoft.hydralab.common.util.PageNode;
 import org.junit.jupiter.api.Assertions;
@@ -13,17 +14,18 @@ import java.util.List;
 class TestCaseGenerationServiceTest extends BaseTest {
 
     @Resource
-    CaseGenerationService caseGenerationService;
+    MaestroCaseGenerationService caseGenerationService;
 
     @Test
     void testParserXMLToPageNode() {
         PageNode rootNode = caseGenerationService.parserXMLToPageNode("src/test/resources/test_route_map.xml");
         Assertions.assertNotNull(rootNode, "parser xml to page node failed");
         rootNode.setPageName("com.microsoft.appmanager");
+        System.out.println(rootNode);
         List<PageNode.ExplorePath> explorePaths = new ArrayList<>();
-        Assertions.assertEquals(explorePaths.size(), 16, "explore path size is not correct");
         caseGenerationService.explorePageNodePath(rootNode, "", "", explorePaths);
-        File caseZipFile = caseGenerationService.generateMaestroCases(rootNode, explorePaths);
+        Assertions.assertEquals(explorePaths.size(), 16, "explore path size is not correct");
+        File caseZipFile = caseGenerationService.generateCaseFile(rootNode, explorePaths);
         Assertions.assertTrue(caseZipFile.exists());
     }
 }
