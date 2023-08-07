e0a79ad39d4f275d4244ef87c2ec771d3fe98c4c
OliverShen <41765758+olivershen-wow@users.noreply.github.com>
2023-08-07 16:23:50 +0800

    Make LLM integration compatible in Hydra Lab for smart test triggering. (#543)
    * Make LLM integration compatible in Hydra Lab for smart test triggering.
    * Add LLM entity

--- agent/src/main/java/com/microsoft/hydralab/agent/config/AppConfiguration.java
+++ agent/src/main/java/com/microsoft/hydralab/agent/config/AppConfiguration.java
@@ -11,6 +11,7 @@ import com.microsoft.hydralab.agent.environment.EnvCapabilityDiscoveryService;
 import com.microsoft.hydralab.agent.runner.smart.SmartTestUtil;
 import com.microsoft.hydralab.agent.service.AgentWebSocketClientService;
 import com.microsoft.hydralab.agent.socket.AgentWebSocketClient;
+import com.microsoft.hydralab.common.entity.agent.LLMProperties;
 import com.microsoft.hydralab.common.file.StorageServiceClientProxy;
 import com.microsoft.hydralab.common.management.AgentManagementService;
 import com.microsoft.hydralab.common.management.listener.DeviceStatusListenerManager;
@@ -29,6 +30,7 @@ import org.springframework.boot.actuate.autoconfigure.metrics.export.prometheus.
 import org.springframework.boot.actuate.metrics.export.prometheus.PrometheusPushGatewayManager;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -212,5 +214,11 @@ public class AppConfiguration {
     @Bean
     public StorageServiceClientProxy storageServiceClientProxy(ApplicationContext applicationContext) {
         return new StorageServiceClientProxy(applicationContext);
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "runner.smart.llm")
+    public LLMProperties llmProperties(){
+        return new LLMProperties();
     }
 }
