6fa8f49fdd206ed0468aa867032270eee0e249e6
Le Zhou <2428499107@qq.com>
2023-08-16 16:47:51 +0800

    format code

--- common/src/main/java/com/microsoft/hydralab/common/appcenter/AppCenterErrorLogHandler.java
+++ common/src/main/java/com/microsoft/hydralab/common/appcenter/AppCenterErrorLogHandler.java
@@ -121,7 +121,7 @@ public class AppCenterErrorLogHandler {
 
         /* Uncaught exception or managed exception. */
         errorLog.setFatal(fatal);
-        if(!fatal){
+        if (!fatal) {
             errorLog.setType(HandledErrorLog.TYPE);
         }
 
