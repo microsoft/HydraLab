dafdc7def2ac4374f1f4f8a52757caea2766608b
BryanSharp <8344245+hydraxman@users.noreply.github.com>
2023-09-08 13:28:26 +0800

    add apk canary exec

--- agent/src/test/java/com/microsoft/hydralab/agent/runner/scanner/ApkCanaryExecutorTest.java
+++ agent/src/test/java/com/microsoft/hydralab/agent/runner/scanner/ApkCanaryExecutorTest.java
@@ -3,7 +3,7 @@ package com.microsoft.hydralab.agent.runner.scanner;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.microsoft.hydralab.common.entity.common.scanner.ApkReport;
-import com.microsoft.hydralab.common.util.FileUtil;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
@@ -12,9 +12,8 @@ public class ApkCanaryExecutorTest {
 
     @Test
     public void testApkCanaryExecutor() {
-        File apkFilePath = new File("build/record_release.apk");
-        FileUtil.downloadFile("https://raw.githubusercontent.com/microsoft/HydraLab/Release/1.12.0/common/src/main/resources/record_release.apk",
-                apkFilePath, null);
+        File apkFilePath = new File("../common/src/test/resources/record_release.apk");
+        Assertions.assertTrue(apkFilePath.exists(), "apk file not exist: " + apkFilePath.getAbsolutePath());
         ApkCanaryExecutor apkCanaryExecutor = new ApkCanaryExecutor();
         ApkReport apkReport = apkCanaryExecutor.analyzeApk(
                 new File("src/main/resources/apk_canary/matrix-apk-canary-2.1.0.jar"),
