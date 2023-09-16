d51de5d8b07e488abc3fea6ff966efa013663fab
Nathan Bu <370788475@qq.com>
2023-09-16 08:31:23 +0800

    Add APK canary as Android package scanner (#596)
    * add apk canary exec
    
    * add apk canary exec
    
    * add apk canary exec
    
    * stop scanning the jar under resources
    
    * new comment
    
    * add --stacktrace param in the build task
    
    * add --stacktrace param in all the build tasks
    
    * add --stacktrace param in all the build tasks
    
    * add sample code and new test case

---
+++ agent/src/test/java/com/microsoft/hydralab/agent/runner/scanner/ApkCanaryExecutorTest.java
@@ -0,0 +1,35 @@
+package com.microsoft.hydralab.agent.runner.scanner;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.microsoft.hydralab.common.entity.common.scanner.ApkReport;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+public class ApkCanaryExecutorTest {
+
+    @Test
+    public void testApkCanaryExecutor() {
+        File apkFilePath = new File("../common/src/test/resources/record_release.apk");
+        Assertions.assertTrue(apkFilePath.exists(), "apk file not exist: " + apkFilePath.getAbsolutePath());
+        ApkCanaryExecutor apkCanaryExecutor = new ApkCanaryExecutor();
+        ApkReport apkReport = apkCanaryExecutor.analyzeApk(
+                new File("src/main/resources/apk_canary/matrix-apk-canary-2.1.0.jar"),
+                new File("src/main/resources/apk_canary/apk_canary_config_template.json"),
+                apkFilePath.getAbsolutePath(),
+                new File("").getAbsolutePath()
+        );
+        System.out.println(JSON.toJSONString(apkReport, SerializerFeature.PrettyFormat));
+    }
+
+    @Test
+    public void testApkReportParsing() {
+        File apkJsonReportFile = new File("src/test/resources/MicrosoftLauncherAPKReport.json");
+        Assertions.assertTrue(apkJsonReportFile.exists(), "apkJsonReportFile does not exist: " + apkJsonReportFile.getAbsolutePath());
+        ApkReport apkReport = ApkCanaryExecutor.getApkReportFromJsonReport(apkJsonReportFile);
+        System.out.println(JSON.toJSONString(apkReport, SerializerFeature.PrettyFormat));
+    }
+
+}
