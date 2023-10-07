2aef23a4a1f069d5fd8d0b5b7f6b111262a9264f
BryanSharp <8344245+hydraxman@users.noreply.github.com>
2023-10-07 23:14:58 +0800

    design test app context

---
+++ common/src/main/java/com/microsoft/hydralab/common/entity/common/TestAppContext.java
@@ -0,0 +1,11 @@
+package com.microsoft.hydralab.common.entity.common;
+
+import java.util.List;
+
+public class TestAppContext {
+    private String packageName;
+    private long packageSize;
+    private String appName;
+    private String appVersion;
+    private List<UIPage> uiPages;
+}
---
+++ common/src/main/java/com/microsoft/hydralab/common/entity/common/UIPage.java
@@ -0,0 +1,5 @@
+package com.microsoft.hydralab.common.entity.common;
+
+public class UIPage {
+    public String name;
+}
