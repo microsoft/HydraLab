d82d59849f3c9969bb68d9041a0ca8a1167ab732
Le Zhou <2428499107@qq.com>
2024-12-02 13:54:20 +0800

    Update AuthController.java

--- center/src/main/java/com/microsoft/hydralab/center/controller/AuthController.java
+++ center/src/main/java/com/microsoft/hydralab/center/controller/AuthController.java
@@ -13,9 +13,13 @@ import com.microsoft.hydralab.common.entity.center.SysUser;
 import com.microsoft.hydralab.common.util.Const;
 import com.microsoft.hydralab.common.util.LogUtils;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.CurrentSecurityContext;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -31,7 +35,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.List;
 
 @RestController
@@ -177,24 +180,27 @@ public class AuthController {
      */
     @GetMapping(value = {"/api/auth/getUserPhoto"}, produces = MediaType.IMAGE_JPEG_VALUE)
     @ResponseBody
-    public void getUserPhoto(@CurrentSecurityContext SysUser requestor,
-                             HttpServletResponse response) {
-        try {
-            InputStream inputStream = null;
-            if (requestor == null || requestor.getAccessToken() == null) {
-                inputStream = FileUtils.class.getClassLoader().getResourceAsStream(Const.Path.DEFAULT_PHOTO);
-            } else {
+    public ResponseEntity<byte[]> getUserPhoto(@CurrentSecurityContext SysUser requestor,
+                                               HttpServletResponse response) {
+        HttpHeaders headers = new HttpHeaders();
+        InputStream inputStream = null;
+        if (requestor == null || requestor.getAccessToken() == null) {
+            inputStream = FileUtils.class.getClassLoader().getResourceAsStream(Const.Path.DEFAULT_PHOTO);
+        } else {
+            try {
                 inputStream = authUtil.requestPhoto(requestor.getAccessToken());
+            } catch (Exception e) {
+                inputStream = FileUtils.class.getClassLoader().getResourceAsStream(Const.Path.DEFAULT_PHOTO);
             }
-            byte[] bytes = new byte[inputStream.available()];
-            inputStream.read(bytes, 0, inputStream.available());
-            response.setContentType(MediaType.IMAGE_JPEG_VALUE);
-            OutputStream out = response.getOutputStream();
-            out.write(bytes);
-            out.flush();
-            out.close();
-        } catch (Exception e) {
-            e.printStackTrace();
         }
+        byte[] bytes = new byte[0];
+        try {
+            bytes = IOUtils.toByteArray(inputStream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        headers.setCacheControl(CacheControl.noCache().getHeaderValue());
+        ResponseEntity<byte[]> responseEntity = new ResponseEntity<>(bytes, headers, HttpStatus.OK);
+        return responseEntity;
     }
 }
