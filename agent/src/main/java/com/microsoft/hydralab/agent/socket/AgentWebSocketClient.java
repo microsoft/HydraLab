709524589550348d35661284b2256a2b5a813dab
Le Zhou <2428499107@qq.com>
2025-04-14 14:40:40 +0800

    Update AgentWebSocketClient.java

--- agent/src/main/java/com/microsoft/hydralab/agent/socket/AgentWebSocketClient.java
+++ agent/src/main/java/com/microsoft/hydralab/agent/socket/AgentWebSocketClient.java
@@ -22,6 +22,7 @@ public class AgentWebSocketClient extends WebSocketClient {
 
     private boolean connectionActive = false;
     private int reconnectTime = 0;
+    private int violatedReconnectTime = 0;
 
     public AgentWebSocketClient(URI serverUri, AgentWebSocketClientService agentWebSocketClientService) {
         super(serverUri);
@@ -54,6 +55,7 @@ public class AgentWebSocketClient extends WebSocketClient {
         agentWebSocketClientService.onMessage(message);
         if (Const.Path.DEVICE_LIST.equals(message.getPath())) {
             reconnectTime = 0;
+            violatedReconnectTime = 0;
         }
     }
 
@@ -65,12 +67,29 @@ public class AgentWebSocketClient extends WebSocketClient {
 
     @Override
     public void onClose(int code, String reason, boolean remote) {
-        log.error("onClose {}, {}, {}", code, reason, remote);
+        log.error("onClose {}, {}, {}, {}", code, reason, remote, reconnectTime);
         reconnectTime++;
         connectionActive = false;
-        // if the connection is closed by server with 1008,1003, exit the agent
-        if (code == CloseReason.CloseCodes.CANNOT_ACCEPT.getCode() || code == CloseReason.CloseCodes.VIOLATED_POLICY.getCode()) {
+        // if the connection is closed by server with 1003, exit the agent
+        if (code == CloseReason.CloseCodes.CANNOT_ACCEPT.getCode()) {
             System.exit(code);
+        }
+
+        // if the connection is closed by server with 1008, wait and retry
+        if (code == CloseReason.CloseCodes.VIOLATED_POLICY.getCode()) {
+            violatedReconnectTime++;
+            if(violatedReconnectTime > 5) {
+                log.error("onClose, code: {}, reason: {}, remote: {}, reconnectTime: {}, {}", code, reason, remote, reconnectTime, violatedReconnectTime);
+                System.exit(code);
+            } else {
+                // wait for 10 seconds and then retry
+                try {
+                    log.info("onClose, code: {}, reason: {}, remote: {}, reconnectTime: {}, {} sleep 10 seconds", code, reason, remote, reconnectTime, violatedReconnectTime);
+                    Thread.sleep(10000);
+                } catch (InterruptedException e) {
+                    log.error("onClose, sleep error", e);
+                }
+            }
         }
     }
 
